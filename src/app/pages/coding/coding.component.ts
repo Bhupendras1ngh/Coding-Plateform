26f6d65c20e621cfda1afdd43cd46b02cef0a64a
Bhupendra <bhupendra@gamil.com>
2023-02-08 09:11:25 +0530

    fiddle working

--- src/app/pages/coding/coding.component.ts
+++ src/app/pages/coding/coding.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class CodingComponent {
 constructor(){}
 ngOnInit(): void{}
-
+fiddle:string ="";
 editorOptions = {theme: 'vs-dark', language: 'javascript'};
 code: string= 'function x() {\nconsole.log("Hello world!");\n}';
 
 run(){
-  console.log("code has been ru!!")
+  console.log("code has been run!!")
 }
 }
